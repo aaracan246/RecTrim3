5b753130f5d9c6158a4f1cb7b9a0a1bf937adbc1
morri <aaracan246@g.educaand.es>
2024-05-16 20:43:46 +0200

    First approachv3

--- src/main/kotlin/dao/GRUPOSdao.kt
+++ src/main/kotlin/dao/GRUPOSdao.kt
@@ -1,12 +1,17 @@
 package dao
 
+import ds.DataSourceFactory
 import entity.GRUPOS
+import inputOutput.Console
 import interfaces.IGROUPSdao
 import java.util.*
+import javax.sql.DataSource
 
-class GRUPOSdao: IGROUPSdao {
+class GRUPOSdao(private val dataSource: DataSource, private val console: Console): IGROUPSdao {
     override fun createGroup(grupos: GRUPOS): GRUPOS? {
-        TODO("Not yet implemented")
+        val sql = "INSERT INTO GRUPOS (ID, GRUPODESC, MEJORPOSCTFID)"
+
+
     }
 
     override fun getAllGroups(): List<GRUPOS>? {
